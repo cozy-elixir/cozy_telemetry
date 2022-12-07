15191d5f81a31a113a2a1398d2d8feed795cc3cc
c4710n <c4710n@users.noreply.github.com>
2022-12-07 19:45:09 +0800

    chore: prepare for publishing

--- mix.exs
+++ mix.exs
@@ -1,13 +1,23 @@
 defmodule CozyTelemetry.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @description "Provides a modular approach to using beam-telemetry packages."
+  @source_url "https://github.com/cozy-elixir/cozy_telemetry"
+
   def project do
     [
       app: :cozy_telemetry,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: @description,
+      source_url: @source_url,
+      homepage_url: @source_url,
+      docs: docs(),
+      package: package(),
+      aliases: aliases()
     ]
   end
 
@@ -22,7 +32,35 @@ defmodule CozyTelemetry.MixProject do
     [
       {:telemetry, "~> 1.0"},
       {:telemetry_metrics, "~> 0.6"},
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false},
       {:telemetry_metrics_statsd, "~> 0.6", only: :test}
     ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_url: @source_url,
+      source_ref: @version
+    ]
+  end
+
+  defp package do
+    [
+      exclude_patterns: [],
+      licenses: ["Apache-2.0"],
+      links: %{GitHub: @source_url}
+    ]
+  end
+
+  defp aliases do
+    [publish: ["hex.publish", "tag"], tag: &tag_release/1]
+  end
+
+  defp tag_release(_) do
+    Mix.shell().info("Tagging release as #{@version}")
+    System.cmd("git", ["tag", @version])
+    System.cmd("git", ["push", "--tags"])
   end
 end
