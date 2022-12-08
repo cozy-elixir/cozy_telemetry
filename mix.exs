cff637931e83ac305b8ab8e63ba3cb0239210566
c4710n <c4710n@users.noreply.github.com>
2022-12-09 00:05:47 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyTelemetry.MixProject do
   use Mix.Project
 
-  @version "0.3.0"
+  @version "0.3.1"
   @description "Provides a modular approach for using beam-telemetry packages."
   @source_url "https://github.com/cozy-elixir/cozy_telemetry"
 
